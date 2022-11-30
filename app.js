72d1eff107a1736d7d87fb608e3907d851b16152
Julie Nisbet <jmnisbet@gmail.com>
2022-11-30 10:58:25 -0800

    initial fetch

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { fetchBlogs } from './fetch-utils.js';
 
 /* Get DOM Elements */
-
+const blogList = document.getElementById('blog-list');
 /* State */
 
 /* Events */
+async function loadBlogs() {
+    const blogs = await fetchBlogs();
+    for (let blog of blogs) {
+        const div = document.createElement('li');
+        const h3 = document.createElement('h3');
+        h3.textContent = blog.title;
+        const p = document.createElement('p');
+        p.textContent = blog.body;
+        div.append(h3, p);
+        blogList.append(div);
+    }
+}
+loadBlogs();
 
 /* Display Functions */
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,29 +1,44 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
+const BASE_URL = 'http://localhost:7890';
 /* Auth related functions */
 
 export function getUser() {
-    return client.auth.user();
+    // return client.auth.user();
 }
 
 export async function signUpUser(email, password) {
-    return await client.auth.signUp({
-        email,
-        password,
-    });
+    // return await client.auth.signUp({
+    //     email,
+    //     password,
+    // });
 }
 
 export async function signInUser(email, password) {
-    return await client.auth.signIn({
-        email,
-        password,
-    });
+    // return await client.auth.signIn({
+    //     email,
+    //     password,
+    // });
 }
 
 export async function signOutUser() {
-    return await client.auth.signOut();
+    // return await client.auth.signOut();
 }
 
 /* Data functions */
+
+export async function fetchBlogs() {
+    const resp = await fetch(`${BASE_URL}/api/v1/blogs`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+    });
+    const data = await resp.json();
+    if (resp.ok) {
+        return data;
+    } else {
+        // eslint-disable-next-line no-console
+        console.error(data.message);
+    }
+}
